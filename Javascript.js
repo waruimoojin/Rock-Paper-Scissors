b21e9e795b781c6fd69e536944113967ff7abecb
waruimoojin <154926913+waruimoojin@users.noreply.github.com>
2024-02-25 16:04:32 +0100

    commit v1 of project

---
+++ Javascript.js
@@ -0,0 +1,59 @@
+function getComputerChoice (){
+    
+    const random = Math.floor(Math.random() * Choice.length)
+
+    const choix = Choice[random]
+
+   
+    return choix;
+
+
+}
+    const Choice = ['rock','paper','siccors']
+    const result = getComputerChoice(Choice)
+console.log(result);
+
+
+function playsSingleRound (playerSelection,computerSelection){
+
+
+
+  if (playerSelection === computerSelection ){
+
+  return `it's a draw`
+}
+   else if (playerSelection ==='rock' && computerSelection ==='siccors'){
+   return 'You win';
+   
+}  
+ else if (playerSelection ==='paper' && computerSelection ==='rock'){
+   return 'You win';
+}
+   else if (playerSelection ==='siccors' && computerSelection ==='paper'){
+   return 'You win';
+}else{
+return 'You lose!'
+
+}
+}
+function playGame(){
+
+    const playerSelection = prompt("Start the game by picking among 'Rock, Paper, Scissors'").toLowerCase();
+    const computerSelection = getComputerChoice();
+console.log(playsSingleRound(playerSelection, computerSelection));
+
+  
+}
+
+playGame()
+playGame()
+playGame()
+playGame()
+playGame()
+playGame()
+
+
+
+
+
+
